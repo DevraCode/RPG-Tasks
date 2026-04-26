e642bb328a83d11ee4e5d0a021fbfa3d03efb5b6
DevraCode <ibuetab@gmail.com>
2026-04-26 23:08:47 +0200

    algunos cambios

---
+++ entrypoints/telegram_bot/handlers_tareas.py
@@ -0,0 +1,22 @@
+from telegram import Update
+from telegram.ext import ContextTypes, ConversationHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InputMedia
+from telegram.ext import CallbackContext
+import hashlib
+
+from core.infrastructure.mysql_repository import MySQLUsuarioRepository
+from core.application.use_cases import VincularIdExternoUseCase
+
+
+from .dbconfig import db_config
+
+repo = MySQLUsuarioRepository(db_config)
+vincular_id_externo_use_case = VincularIdExternoUseCase(repo)
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------
+
+#INSERTAR TAREAS
