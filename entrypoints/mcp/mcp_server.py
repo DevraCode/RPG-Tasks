19fe9db6c0c7d0b4bc05decc5f94ea9ad1107696
DevraCode <ibuetab@gmail.com>
2026-04-29 23:24:58 +0200

    Integracion de un servidor MCP

--- entrypoints/mcp/mcp_server.py
+++ entrypoints/mcp/mcp_server.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-# Añade la raíz del proyecto al PYTHONPATH
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
 
@@ -11,14 +11,13 @@ from core.application.use_cases import TareasUseCase
 from core.infrastructure.mysql_tareas_repository import MySQLTareasRepository
 from core.infrastructure.dbconfig import db_config
 
-# 1. Inicializar FastMCP
 mcp = FastMCP("RPG-Tasks-Server")
 
-# 2. Configurar dependencias
+
 repo = MySQLTareasRepository(db_config)
 tareas_logic = TareasUseCase(repo)
 
-# 3. Exponer una herramienta (Tool)
+
 @mcp.tool()
 def crear_nueva_tarea(id_usuario: int, nombre: str) -> str:
     """
